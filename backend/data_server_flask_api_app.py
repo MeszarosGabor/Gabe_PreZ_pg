e89386a19c989ed5c3abbc3a7e5e86d64b5e2a5a
Gabor Meszaros <jegkocka88@gmail.com>
2021-05-13 11:47:13 +0200

    Added Flask JSON API server implementation.
    - supports basic queries (all data, entry by title)
    - SORT is lexicographical for now, needs fixing (convert to datetime)
    - manually tested the endpoints

---
+++ backend/data_server_flask_api_app.py
@@ -0,0 +1,59 @@
+"""
+PreziMetadata  Service implementaton.
+"""
+
+import json
+
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class PreziMetadata(Resource):
+    """ Metadata service implementation. """
+    def __init__(self, data_source='../data_source/prezis.json'):
+        self.data_source = data_source
+        self.json_data = None
+        self.sorted_json_data = None  # only sort it once
+
+        self.initialize()
+    # end __init__()
+
+    def initialize(self):
+        """ Loads datasource. """
+        with open(self.data_source, 'r') as handle:
+            self.json_data = json.load(handle)
+        self.sorted_json_data = sorted(self.json_data,
+                                       key=lambda entry: entry['createdAt'])
+    # end initialize()
+
+    def get(self):
+        """
+            Get metadata for a single item. Default for all data returned.
+            Return all entries in either sorted or unsorted fashion
+            depending on the corresponding flag.
+        """
+        title = request.args.get('title', '')
+        is_sorted = request.args.get('sorted', False)
+        print(request.args)
+        if not title:
+            if is_sorted:
+                return self.sorted_json_data
+            return self.json_data
+        for entry in self.json_data:
+            if entry['title'] == title:
+                return entry
+        return {'result': 'not found'}
+    # end get()
+
+
+api.add_resource(PreziMetadata,
+                 "/search",
+                 "/search/",
+                 )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=18871, debug=True)
