82060fe0e2866323c3264a47fb1a2f25f4fcf038
Gabor Meszaros <jegkocka88@gmail.com>
2021-05-14 11:38:11 +0200

    fixed CORS issue

--- backend/run_flask_server_app.py
+++ backend/run_flask_server_app.py
@@ -3,11 +3,13 @@ PreziMetadata Flask Service main runner.
 """
 
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 
 from data_server_flask_api_class import PreziMetadata
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(PreziMetadata,
